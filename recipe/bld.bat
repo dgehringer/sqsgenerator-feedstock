93e7919857c3c71943bc9e028e8c64749a7648ac
dgehringer <35268573+dgehringer@users.noreply.github.com>
2025-07-27 15:11:11 +0200

    try call instead of direct

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,7 +4,7 @@
 :: https://github.com/conda-forge/numpy-feedstock/blob/main/recipe/bld.bat
 
 git clone https://github.com/microsoft/vcpkg.git
-vcpkg/bootstrap-vcpkg.bat
+call vcpkg/bootstrap-vcpkg.bat
 
 :: -wnx flags mean: --wheel --no-isolation --skip-dependency-check
 %PYTHON% -m build -w -n -x python\
