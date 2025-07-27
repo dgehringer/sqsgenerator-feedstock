58a7055d000d5da092ee8c5868171fb9600f41db
dgehringer <35268573+dgehringer@users.noreply.github.com>
2025-07-27 17:37:15 +0200

    set target triplet for ppcle

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,17 @@ git clone https://github.com/microsoft/vcpkg.git
 vcpkg/bootstrap-vcpkg.sh
 
 export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-export VCPKG_FORCE_SYSTEM_BINARIES=1
+
+if [[ "${TARGET_PLATFORM}" == "linux-ppc64le" ]]; then
+    export VCPKG_TARGET_TRIPLET="ppc64le-linux"
+elif [[ "${TARGET_PLATFORM}" == "osx-arm64" ]]; then
+    export VCPKG_TARGET_TRIPLET="arm64-osx"
+elif [[ "${TARGET_PLATFORM}" == "osx-64" ]]; then
+   export VCPKG_TARGET_TRIPLET="x64-osx"
+fi
+
+echo "Platform ${TARGET_PLATFORM} -> ${VCPKG_TARGET_TRIPLET}"
+
 $PYTHON -m build -w -n -x python/
 
 $PYTHON -m pip install python/dist/sqsgenerator*.whl
