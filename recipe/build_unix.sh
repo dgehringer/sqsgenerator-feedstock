5c7d5b33c44b7529361458dbaf98a95f3448fa8a
dgehringer <35268573+dgehringer@users.noreply.github.com>
2025-07-27 13:57:52 +0200

    try bld.bat for windows

--- recipe/build_unix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-git clone https://github.com/microsoft/vcpkg.git
-vcpkg/bootstrap-vcpkg.sh
-
-export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-$PYTHON -m pip install python/ -v --no-deps --no-build-isolation
