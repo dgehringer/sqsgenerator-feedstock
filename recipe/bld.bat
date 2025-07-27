25ef1ad165be90ff2fe38cc3c3c960f7e0fd8891
dgehringer <35268573+dgehringer@users.noreply.github.com>
2025-07-27 14:37:22 +0200

    use scikit win build

---
+++ recipe/bld.bat
@@ -0,0 +1,19 @@
+@echo on
+
+:: Inspired by the numpy-feedstock build script:
+:: https://github.com/conda-forge/numpy-feedstock/blob/main/recipe/bld.bat
+
+git clone https://github.com/microsoft/vcpkg.git
+vcpkg/bootstrap-vcpkg.bat
+
+:: -wnx flags mean: --wheel --no-isolation --skip-dependency-check
+%PYTHON% -m build -w -n -x python\
+
+if %ERRORLEVEL% neq 0 exit 1
+
+:: `pip install dist\*.whl` does not work on windows,
+:: so use a loop; there's only one wheel in dist/ anyway
+for /f %%f in ('dir /b /S .\dist') do (
+    pip install %%f
+    if %ERRORLEVEL% neq 0 exit 1
+)
